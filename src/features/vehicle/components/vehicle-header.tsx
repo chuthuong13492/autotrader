3da7487b1990668d26d7f9d06a5a68a08113ac82
Nguyễn Hiếu Kiên <kien.nh2602@gmail.com>
2025-10-07 19:41:56 +0700

    fix: VDP header

--- src/features/vehicle/components/vehicle-header.tsx
+++ src/features/vehicle/components/vehicle-header.tsx
@@ -1,25 +1,13 @@
 import { Header } from '@/components/layout/header'
 import { ProfileDropdown } from '@/components/profile-dropdown'
-import { DynamicBreadcrumb } from '@/components/dynamic-breadcrumb'
-import { useParams } from '@tanstack/react-router'
-import { ALL_CARS } from '@/features/dashboard/data/mock-data'
+import { MainNavigation } from '@/components/layout/main-navigation'
 
 export function VehicleHeader() {
-  const { id } = useParams({ from: '/_dashboard/vehicle/$id' })
-
-  
-  // Find the vehicle by ID
-  const vehicle = ALL_CARS.find(car => car.id === id)
-
   return (
     <Header>
-      <DynamicBreadcrumb 
-        lastItem={{
-          label: `${vehicle?.make} ${vehicle?.model}`,
-          href: undefined
-        }}
-      />
-      
+      {/* MAIN NAVIGATION */}
+      <MainNavigation className='hidden @[1300px]:flex' />
+      {/* PROFILE */}
       <div className='ms-auto flex items-center space-x-4'>
         <ProfileDropdown />
       </div>
